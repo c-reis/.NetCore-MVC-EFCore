9d5ec9cbb4a67b9fb74714656d5615b483f71992
Carlos Reis <carlosreis11@hotmail.com>
2021-03-14 15:48:06 -0300

    Adicionado a classe EndCidadeController

---
+++ Controllers/EndCidadeController.cs
@@ -0,0 +1,59 @@
+﻿using AppCardapio.Data;
+using AppCardapio.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCardapio.Controllers
+{
+    public class EndCidadeController : Controller
+    {
+        private readonly AppCardapioDbContext _context;
+        public EndCidadeController(AppCardapioDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            List<EndCidadeModel>listaCidades = new List<EndCidadeModel>();
+            listaCidades = await _context.EndCidade.AsNoTracking().ToListAsync();
+            return View(listaCidades);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(EndCidadeModel novaCidade)
+        {
+            _context.EndCidade.Add(novaCidade);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Update(int idCidade)
+        {
+            return View(await _context.EndCidade.Where(c => c.Id_cidade == idCidade).FirstOrDefaultAsync());
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult> Update(EndCidadeModel alteraCidade)
+        {
+            _context.Entry(alteraCidade).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            
+            return View();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int idCidade)
+        {
+            EndCidadeModel cidade = await _context.EndCidade.Where(c => c.Id_cidade == idCidade).FirstOrDefaultAsync();
+            _context.EndCidade.Remove(cidade);
+            return RedirectToAction("Index");
+        }
+    }
+}
